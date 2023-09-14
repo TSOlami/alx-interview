72c908ab6503c140a66579837d21c4dbe5c1fd81
TSOlami <tijani.saheed@yahoo.com>
2023-09-14 14:05:36 +0100

    fix pep8 style

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,6 +3,7 @@
 Rotate 2D Matrix
 """
 
+
 def rotate_2d_matrix(matrix):
     """
     Rotates a 2D matrix 90 degrees clockwise in-place.
@@ -17,4 +18,3 @@ def rotate_2d_matrix(matrix):
     # Reverse each row
     for i in range(n):
         matrix[i].reverse()
-
