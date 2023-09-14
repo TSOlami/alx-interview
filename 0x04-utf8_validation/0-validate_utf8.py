e824b699976fc1203293cefb111fb2f832ce62ac
TSOlami <tijani.saheed@yahoo.com>
2023-09-14 13:45:27 +0100

    fix merge

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -15,9 +15,6 @@ only need to handle the 8 least significant bits of each integer
 
 
 def validUTF8(data):
-<<<<<<< HEAD
-    pass
-=======
     """Determine if a given data set represents a valid UTF-8 encoding.
 
     Args:
@@ -71,4 +68,3 @@ def validUTF8(data):
         return True
     else:
         return False
->>>>>>> 1d1e25168fa4af8ca7315d9af5ead0f38455ed39
