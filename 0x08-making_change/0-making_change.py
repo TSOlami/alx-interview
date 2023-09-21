9648234e0f586ee882bec1273f8b77e02202a60e
TSOlami <tijani.saheed@yahoo.com>
2023-09-21 12:30:56 -0700

    fix pep8

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,6 +2,7 @@
 """Dynamic Programming
 """
 
+
 def makeChange(coins, total):
     “””The make change module
     ”””
@@ -9,19 +10,19 @@ def makeChange(coins, total):
         return 0
 
     # Initialize a table to store the minimum number
-	# of coins needed for each amount from 0 to total
+        # of coins needed for each amount from 0 to total
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
     # Calculate the minimum number of coins needed
-	# for each amount from 1 to total
+    # for each amount from 1 to total
     for amount in range(1, total + 1):
         for coin in coins:
             if coin <= amount:
                 dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
     # If dp[total] is still float('inf'),
-	# it means the total cannot be met
+        # it means the total cannot be met
     if dp[total] == float('inf'):
         return -1
 
